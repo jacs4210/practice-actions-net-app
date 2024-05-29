c4a3554a784c1e702bbb8f345a3b4e0c36836982
Jairo Cuartas <jairo.cuartas18@gmail.com>
2024-05-29 12:20:26 -0500

    Merge

--- obj/Debug/net8.0/newWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/newWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("newWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c1df7779fdb7625e9b2eaa9339aa554cdcecd8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("newWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("newWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
